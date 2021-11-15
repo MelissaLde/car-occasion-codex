1b05fc5f8faf20dcd1a996a42f575bd3284a2697
m-linde <linde.melissa34@gmail.com>
2021-11-15 21:37:15 +0100

    feat(layout): create app layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="app">Hello Word</div>
+  <div id="App" class="grid">
+    <header class="bg-red-600 col-span-12">header</header>
+
+    <nav class="bg-yellow-200 col-span-4">nav</nav>
+
+    <main class="bg-blue-400 col-span-8">main</main>
+  </div>
 </template>
 
 <script>
@@ -7,3 +13,9 @@ export default {
   name: 'App',
 }
 </script>
+
+<style>
+nav {
+  height: calc(100vh - 24px);
+}
+</style>
