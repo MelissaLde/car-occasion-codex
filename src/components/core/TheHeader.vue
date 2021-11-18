c57b353390e07b4ec05353f67966b37006307e31
m-linde <linde.melissa34@gmail.com>
2021-11-18 20:05:34 +0100

    feat(VCarCard): finish implement VCarCard

--- src/components/core/TheHeader.vue
+++ src/components/core/TheHeader.vue
@@ -1,11 +1,7 @@
 <template>
   <header class="bg-black fixed w-full top-0">
     <div class="container mx-auto py-1">
-      <img
-        src="https://lh3.googleusercontent.com/proxy/DAqRhXGe4gyCVrpXVv7KTBveLViIy5HXAjhm9SD5DKmZEU1RT8lxPi0rdGvf4P3Nocbg7OWwivt9h2cMei2-SRbp6thaqAWUKUl_tivanBQ0t7pB"
-        width="150px"
-        alt="Logo"
-      />
+      <img src="@/assets/images/loogsvg.png" width="150px" alt="Logo" />
     </div>
   </header>
 </template>
